359964a378409db460e822f1c7c855e4b03877f5
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-02-06 09:58:33 +0100

    docs: Added documentation for loadCatalog header
    Included comprehensive documentation for function declaration

--- src/services/catalogLoader/loadCatalog.h
+++ src/services/catalogLoader/loadCatalog.h
@@ -4,6 +4,10 @@
 #pragma once
 #include "../../classes/Catalog/Catalog.h"
 
+/**
+ * @brief Loads the product data from a CSV file and constructs a Catalog object.
+ * @return Catalog A Catalog object fully populated with Product data from the CSV file.
+ */
 Catalog loadCatalog();
 
 #endif //ECOMMERCECORE_LOADCATALOG_H
