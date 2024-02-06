701d626b8e949f107da22095a8a33c85b51c546b
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-02-06 09:52:30 +0100

    feat: Implement loadCatalog header for product list initialization
    Introduced relevant folder structure
    Function will load and parse data from CSV source

---
+++ src/services/catalogLoader/loadCatalog.h
@@ -0,0 +1,9 @@
+#ifndef ECOMMERCECORE_LOADCATALOG_H
+#define ECOMMERCECORE_LOADCATALOG_H
+
+#pragma once
+#include "../../classes/Catalog/Catalog.h"
+
+Catalog loadCatalog();
+
+#endif //ECOMMERCECORE_LOADCATALOG_H
