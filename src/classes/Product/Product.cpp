8a05f5e7a33b7fc67cfaf132cb25323ba7f1632e
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-02-06 08:25:04 +0100

    Add Product class implementation in source file
    Introduced the Product.cpp file, writing implementation to all the methods

--- src/classes/Product/Product.cpp
+++ src/classes/Product/Product.cpp
@@ -1,5 +1,19 @@
-//
-// Created by Sviatoslav Kushnierov on 05/02/2024.
-//
-
 #include "Product.h"
+
+std::string Product::getProductName()      { return productName; }
+
+double Product::getProductPrice()          { return productPrice; }
+
+uint16_t Product::getProductID()           { return productID; }
+
+std::string Product::getProductCategory()  { return productCategory; }
+
+std::ostream &operator << (std::ostream &out, Product &object)
+{
+    out << "Name: " << object.getProductName() << ", ";
+    out << "ID: " << object.getProductID() << ", ";
+    out << "Category: " << object.getProductCategory() << ", ";
+    out << "Price: " << object.getProductPrice() << std::endl;
+    return out;
+
+}
