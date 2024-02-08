db655ae5643b16bf595d3088e4996f2230fba67b
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-02-08 08:59:16 +0100

    Test loadCatalog() and print_catalog()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+#include "classes/Catalog/Catalog.h"
+#include "services/catalogLoader/loadCatalog.h"
 
 int main()
 {
+    Catalog catalog = loadCatalog();
+    catalog.print_catalog();
 
     return 0;
 }
