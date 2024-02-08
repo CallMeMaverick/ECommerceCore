212d49b1eb974ef670b2ba127a31e73197d11871
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-02-08 09:08:30 +0100

    chore: Minor docs and style changes

--- src/services/catalogLoader/loadCatalog.cpp
+++ src/services/catalogLoader/loadCatalog.cpp
@@ -9,8 +9,6 @@ class Product;
 // forward declaration for Catalog class
 class Catalog;
 
-// forward declaration for the function to add a product to the database
-void push_to_database(Product product);
 
 Catalog loadCatalog()
 {
@@ -51,8 +49,7 @@ Catalog loadCatalog()
         getline(ss, token, ',');
         product.productPrice = stof(token);
 
-        // Here you'd call a method or function to add the product to the catalog.
-        // This function or method should be defined elsewhere in your code.
+
         catalog.push_to_database(product);
     }
 
