7154007b41954ad2fb2e68148b658dd99d4ee98d
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-02-08 10:22:23 +0100

    feat: Add authentication namespace declaration
    Introduced implementation for authentication namespace for managing accounts

---
+++ src/authentication/authentication.h
@@ -0,0 +1,25 @@
+#ifndef ECOMMERCECORE_AUTHENTICATION_H
+#define ECOMMERCECORE_AUTHENTICATION_H
+
+#pragma once
+#include "string"
+
+namespace authentication
+{
+    /**
+     * @brief Registers a new user by adding their credentials to a JSON file.
+     * @param username The username of the new user. It must be unique among all users.
+     * @param password The password for the new user. It should meet the security criteria.
+     */
+    void singUp(const std::string &username, const std::string &password);
+
+    /**
+     * @brief Authenticates a user by comparing the provided credentials against those in a JSON file.
+     * @param username The username of the user attempting to log in.
+     * @param password The password provided by the user for login.
+     */
+    void logIn(const std::string &username, const std::string &password);
+}
+
+
+#endif //ECOMMERCECORE_AUTHENTICATION_H
