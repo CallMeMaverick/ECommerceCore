67b2ebb71384a8e9a00ebb367945f5a7430b3cd4
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-02-14 21:31:02 +0100

    feat: Add implementation of sortByCategory() for Catalog class
    Implemented sortByCategory() for sorting vector of products by category

--- src/classes/Catalog/Catalog.cpp
+++ src/classes/Catalog/Catalog.cpp
@@ -4,12 +4,13 @@ void Catalog::push_to_database(const Product &product) { catalog.push_back(produ
 
 void Catalog::print_catalog()
 {
-    for (auto& product : catalog)
-    {
-        std::cout << "Name: " << product.getProductName() << ", ";
-        std::cout << "ID: " << product.getProductID() << ", ";
-        std::cout << "Category: " << product.getProductCategory() << ", ";
-        std::cout << "Price: " << product.getProductPrice() << std::endl;
+    for (auto& category : this->sortByCategory()) {
+        std::cout << "\n\nCategory: " << category.first << std::endl;
+        for (auto& product : category.second) {
+            std::cout << "Product ID: " << product.getProductID()
+                      << ", Name: " << product.getProductName()
+                      << ", Price: $" << product.getProductPrice() << std::endl;
+        }
     }
 }
 
@@ -18,4 +19,18 @@ Product& Catalog::operator[](int index)
     if (index < 0) return catalog[0];
     else if (index > catalog.size() - 1) return catalog[catalog.size() - 1];
     else return catalog[index];
+}
+
+std::map<std::string, std::vector<Product>> Catalog::sortByCategory()
+{
+    // Create a map to store products categorized by their category name.
+    std::map<std::string, std::vector<Product>> productsByCategory;
+    // Traverse the entire catalog vector that contains the initial set of products.
+    for (auto& product : catalog) {
+        // Access (or create if it doesn't exist) the vector of products for the current product's category,
+        // and add the current product to this vector. The map key is the product's category,
+        // and the value is a vector containing all products within that category.
+        productsByCategory[product.getProductCategory()].push_back(product);
+    }
+    return productsByCategory;
 }
