5946a5367d3f2facf3f97a0f862535923faeed82
Kushnierov Sviatoslav <ksvyat21@gmail.com>
2024-02-17 12:59:15 +0100

    feat: Introduced startAuthentication class declaration
    Declaration for class that will handle authentication at the beginning

---
+++ src/UI/menu/authenticationProcess/startAuth/startAuthentication.h
@@ -0,0 +1,13 @@
+#ifndef ECOMMERCECORE_STARTAUTHENTICATION_H
+#define ECOMMERCECORE_STARTAUTHENTICATION_H
+
+
+class startAuthentication
+{
+public:
+    static bool execute();
+    ~startAuthentication() = default;
+};
+
+
+#endif //ECOMMERCECORE_STARTAUTHENTICATION_H
