318881f4296c75ba44c896873ef13308a7b047a1
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-02-18 12:23:33 +0100

    refactor: Implement changes in logic authentication works
    Implemented changes in authentication logic

--- src/UI/menu/authenticationProcess/startAuth/startAuthentication.cpp
+++ src/UI/menu/authenticationProcess/startAuth/startAuthentication.cpp
@@ -6,7 +6,7 @@
 #include <string>
 
 
-bool startAuthentication::execute()
+AuthenticationResult startAuthentication::execute()
 {
     // Variable for option chose by the user
     uint16_t optionChosen;
@@ -24,7 +24,7 @@ bool startAuthentication::execute()
     }
 
     if (optionChosen == 0)  // <-- stop executing and return false
-        return false;
+        return { false, {} };
     else if (optionChosen == 1)
     {
         std::cout << "\nUsername: ";
@@ -39,14 +39,15 @@ bool startAuthentication::execute()
             if (authentication::logIn(username, password))
                 std::cout << GREEN_COLOR <<  "Successfully logged in" << RESET_COLOR << std::endl;
 
+            Customer customer = { username, password };
             // if exception hasn't yet been thrown, return true and stop the executing
-            return true;
+            return { true, customer };
         }
         catch (const std::invalid_argument& exp)
         {
             // return false otherwise
             std::cout << RED_COLOR <<  "---> " << exp.what() << " <---" << RESET_COLOR << std::endl;
-            return false;
+            return { false, {} };
         }
     }
     else if (optionChosen == 2)
@@ -79,23 +80,24 @@ bool startAuthentication::execute()
             if (authentication::signUp(username, password))
                 std::cout << GREEN_COLOR <<  "Successfully signed up" << RESET_COLOR << std::endl;
 
+            Customer customer = { username, password };
             // if exception hasn't yet been thrown, return true and stop the executing
-            return true;
+            return { true, customer };
         }
         catch (const std::invalid_argument& exp)      // <-- process invalid_argument exception
         {
             std::cout << RED_COLOR <<  "---> " << exp.what() << " <---" << RESET_COLOR << std::endl;
-            return false;
+            return { false, {} };
         }
         catch (const std::runtime_error& exp)         // <-- process runtime_error exception
         {
             std::cout << RED_COLOR <<  "---> " << exp.what() << " <---" << RESET_COLOR << std::endl;
-            return false;
+            return { false, {} };
         }
         catch (nlohmann::json::parse_error& jsonExp)  // <-- process json::parse_error exception
         {
             std::cout << RED_COLOR <<  "---> " << jsonExp.what() << " <---" << RESET_COLOR << std::endl;
-            return false;
+            return { false, {} };
         }
 
         /*
