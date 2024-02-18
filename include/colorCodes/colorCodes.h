e475f0f0e07190f449bb997c0a5ea388833a5117
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-02-18 10:31:48 +0100

    chore: Add header with color directives

---
+++ include/colorCodes/colorCodes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/* directives for text formatting (ANSI escape codes) */
+#define RED_COLOR     "\x1b[31m"
+#define GREEN_COLOR   "\x1b[32m"
+#define MAGENTA_COLOR "\x1b[35m"
+#define BOLD_TEXT     "\x1b[1m"
+#define RESET_COLOR   "\x1b[0m"
