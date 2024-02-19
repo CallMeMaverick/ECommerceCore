64243f1c71873c8f0dce40c6fad566d61d7e4471
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-02-19 13:23:19 +0100

    refactor: Add item number when printing catalog

--- src/services/mapTraverser/iterateOver.cpp
+++ src/services/mapTraverser/iterateOver.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include "../../../include/colorCodes/colorCodes.h"
 
 void iterateOver(std::map<std::string, std::vector<Product>>& map)
 {
+    uint32_t counter = 1;
     for (auto& category : map) {
         for (auto& data : category.second) {
+            std::cout << GREEN_COLOR << counter++ << ") " << RESET_COLOR;
             std::cout << "Product ID: " << data.getProductID()
                       << ", Name: " << data.getProductName()
                       << ", Price: $" << data.getProductPrice() << std::endl;
