29b14557f413e915f6562bcb2f851a8f437ee540
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-02-21 00:07:14 +0100

    docs: Documentation for MenuManager implementation

--- src/UI/menu/MenuManager/MenuManager.cpp
+++ src/UI/menu/MenuManager/MenuManager.cpp
@@ -11,37 +11,45 @@
 
 void MenuManager::execute()
 {
+    // Authentication process, returns struct { boolean, CustomerObject }
     auto auth = startAuthentication::execute();
-    if (auth.result)
+    if (auth.result)  // if authentication is successful
     {
-        Catalog                                      catalog;
-        int                                          entry;
-        std::string                                  category;
-        std::map<std::string, std::vector<Product>>  mappedCatalog;
+        // Initialize all variables required for the execution
+        Catalog                                      catalog;                   // <-- Catalog object to have full products loaded
+        int                                          entry;                     // <-- Entry to manage
+        std::string                                  category;                  // <-- Category to sort catalog by
+        std::map<std::string, std::vector<Product>>  mappedCatalog;             // <-- Mapped Catalog to store map object of categorized catalog
 
-        Customer                                     customer = *auth.customer;
-        bool                                         continueExecution = true;
+        Customer                                     customer = *auth.customer; // <-- Customer object from authentication
+        bool                                         continueExecution = true;  // <-- Flag for while loop
 
         while (continueExecution)
         {
+            // Get the entry from the user
             entry = manageInitialEntries::execute();
+            // Load catalog
             catalog = loadCatalog();
 
-            if (entry == 1)
+            if (entry == 1)  // <-- process the entry obtained
             {
+                // Get the category to categorize catalog by from the user
                 category = manageInitialEntries::manageOptionShop();
+                // Create categorized map object of products
                 mappedCatalog = catalog.sortByCategory(category);
+                // Print categorized map object of products
                 std::cout << std::endl;
                 iterateOver(mappedCatalog);
 
+                // Process further actions done on catalog
                 manageAction::execute(customer, mappedCatalog, category);
             }
             else if (entry == 2)
-                manageInitialEntries::manageBasket(customer);
+                manageInitialEntries::manageBasket(customer);                                    // <-- Manage options for basket
             else if (entry == 3)
-                manageInitialEntries::manageOptionManageAccount(customer, continueExecution);
+                manageInitialEntries::manageOptionManageAccount(customer, continueExecution); // <-- Manage options for account
             else if (entry == 4)
-                continueExecution = false;
+                continueExecution = false;                                                          // <-- Stop the execution
         }
     }
 
